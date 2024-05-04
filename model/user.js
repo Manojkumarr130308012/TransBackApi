30b0bab6f9eb3515db0ed8708a160700129aefac
manojkumar <manojkumarr21@gmail.com>
2024-05-04 14:28:29 +0530

    testing

--- model/user.js
+++ model/user.js
@@ -37,7 +37,7 @@ const userSchema = new mongoose.Schema({
     },
     userLocation: {
         type: String,
-        required: true,
+        required: false,
     },
     godown: {
         type: String,
